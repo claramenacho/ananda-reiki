e1068844130afd42aa883ee76da17669a0ddaddb
claramenacho <menachomc@hotmail.com>
2026-05-07 17:17:04 -0300

    cambio_ en_el_send_email_para_render

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -38,7 +38,7 @@ export const Login = () => {
         e.preventDefault();
         try {
             const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
-            const response = await fetch('http://localhost:5000/api/auth/forgot-password', {
+            const response = await fetch(`${baseURL}/api/auth/forgot-password`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email })
