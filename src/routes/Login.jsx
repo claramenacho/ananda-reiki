14c90c8bbcfa6b4675aac3bec66e2514d4ed09e5
claramenacho <menachomc@hotmail.com>
2026-05-07 17:14:55 -0300

    arreglo_del_login_para_render

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -15,7 +15,7 @@ export const Login = () => {
         e.preventDefault();
         try { // <-- Faltaba este try
             const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
-            const response = await fetch('http://localhost:5000/api/auth/login', {
+            const response = await fetch(`${baseURL}/api/auth/login`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password }) 
